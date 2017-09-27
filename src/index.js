c57c5a904ec668a62e5492567a8adbab11d517d8
LiudmilaTrafimuk <TrLuda@tut.by>
2017-09-27 11:20:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var s;
+ for (let i = str.length - 1; i >= 0; i--){
+	for (let j = 0; j < bracketsConfig.length; j++){
+	  	s = bracketsConfig[j].join('');
+	    str = str.replace(s,"");
+	  }
+}  
+	if (str == ""){
+		return true;
+	}
+	return false;
 }
